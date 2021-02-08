03356563812f37f4ac378af26238b07875fcba18
Lauris <la.stepanovs@gmail.com>
2021-02-08 16:15:01 +0200

    master: initialize commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Waff
+
+Advertisment webside, to buy or sell things :)
+
+##Project setup:
+
+###Setup client
+
+Go to client folder:
+
+```
+cd client
+```
+
+Install all needed dependencies.
+
+```
+npm install
+```
+
+Run development server.
+
+```
+npm run serve
+```
+
+By defult it will be runed on the [http//localhost:8080/](http//localhost:8080/)
+
+###Setup server
+
+Go to server folder:
+
+```
+cd server
+```
+
+Install all needed server dependencies.
+
+```
+composer install
+```
+
+Run development server.
+
+```
+php artisan serve
+```
+
+By defult it will be runed on the [http://127.0.0.1:8000](http://127.0.0.1:8000)
---
+++ server/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', "min:8", 'confirmed']
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return $user->createToken('Auth Token')->accessToken;
+    }
+}
