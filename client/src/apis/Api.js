8c462c69defa1fbcd50b6f27eb4734c3704207c4
Lauris <la.stepanovs@gmail.com>
2021-03-24 00:30:54 +0200

    updated baseURL

--- client/src/apis/Api.js
+++ client/src/apis/Api.js
@@ -4,7 +4,7 @@ axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 
 const BaseApi = axios.create({
-    baseURL: " http://127.0.0.1:8000/api"
+    baseURL: "http://127.0.0.1:8000/api"
 });
 
 const Api = () => {
