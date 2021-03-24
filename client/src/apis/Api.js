624e9de614bf37dcc79ce0cd028f75c31feb89b7
Lauris <la.stepanovs@gmail.com>
2021-03-24 21:52:42 +0200

    updated baseURL

--- client/src/apis/Api.js
+++ client/src/apis/Api.js
@@ -4,7 +4,7 @@ axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 
 const BaseApi = axios.create({
-    baseURL: "https://sirual.site:8000/api/posts/api"
+    baseURL: "https://sirual.site:8000/api"
 });
 
 const Api = () => {
