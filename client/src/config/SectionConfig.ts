65dceb89af159825c5b5cd1bfd5954effc112498
Lauris <la.stepanovs@gmail.com>
2021-04-14 22:41:38 +0300

    master: added menu config

---
+++ client/src/config/SectionConfig.ts
@@ -0,0 +1,37 @@
+interface AdMenu {
+  name: string;
+  child: Array<AdMenu> | Array<string>;
+}
+
+const config: Array<AdMenu> = [
+    {
+      name: "cars",
+      child: [
+          {
+        name: "basic",
+        child: [
+          {
+            name: "Audi",
+            child: [
+              "A4",
+              "A5",
+              "A6"
+            ]
+          }
+        ]
+      },
+        {
+          name: "kravas",
+          child: [
+            "Audi"
+          ]
+        }
+      ]
+    },
+    {
+      name: "phones",
+      child:[
+          "Apple"
+      ]
+    }
+];
