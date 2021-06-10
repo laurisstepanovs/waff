63a9ab6ae9164f55548e9f89bed953408c51f381
Lauris <la.stepanovs@gmail.com>
2021-06-11 01:27:37 +0300

    master: fixed some bugs and added menu generation

---
+++ server/database/migrations/2021_05_23_105403_create_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string("sec1", 255)->nullable();
+            $table->string("sec2", 255)->nullable();
+            $table->string("sec3", 255)->nullable();
+            $table->string("sec4", 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
